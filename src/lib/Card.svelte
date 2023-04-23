76fb259985261fc1c52478e43709570b02ee9fda
LeoSM_07 <69401930+LeoSM-07@users.noreply.github.com>
2023-04-22 21:47:59 -0700

    Add `lib`

---
+++ src/lib/Card.svelte
@@ -0,0 +1,16 @@
+<div class="card">
+  <slot></slot>
+</div>
+
+
+<style>
+	.card {
+		background: white;
+		border: solid var(--slate-200);
+		border-width: 1px;
+		border-radius: 12px;
+		display: inline-block;
+		width: 100vw;
+		margin-inline: 2rem;
+	}
+</style>
---
+++ src/lib/ColorChip.svelte
@@ -0,0 +1,32 @@
+<script>
+  /** @type {number} */
+  export let index;
+  /** @type {string} */
+  export let range;
+  
+
+  import cssVars from 'svelte-css-vars';
+  import { colors } from "../routes/curver";
+
+  $: styleVars = {
+    bgColor: colors[index],
+  };
+</script>
+
+<p class="chip" use:cssVars={styleVars}>
+  {range}
+</p>
+
+
+<style>
+  .chip {
+    color: white;
+    background: var(--bgColor);
+    border-radius: 100px;
+    margin-inline: 2rem;
+    padding: 5px;
+    padding-inline: 10px;
+    text-align: center;
+    width: 75px;
+  }
+</style>
